3ec4e53b0ec1af9ec92c867ace1c2d215a9f46ce
liuzicheng <liuzicheng97@163.com>
2019-11-02 22:04:57 +0800

    11.2号代码

---
+++ src/components/goods/GoodsList.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="goodslist-container">
+    <!-- 这是图片列表部分 -->
+    <div class="goods-list">
+      <div class="goods-item" v-for="item in goodsList" :key="item.id">
+        <img :src="item.img_url" alt="">
+        <div class="info">
+          <h3 class="title">{{ item.title }}</h3>
+          <div class="price">
+            <span class="new">¥{{ item.sell_price }}</span>
+            <span class="old">¥{{ item.market_price }}</span>
+          </div>
+          <div class="hot">
+            <span>热卖中</span>
+            <span>剩余{{ item.stock_quantity }}件</span>
+          </div>
+        </div>
+      </div>
+
+
+      <mt-button type="danger" size="large" plain @click="getmore">加载更多</mt-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      goodsList: [], // 商品列表数据
+      pageIndex: 1,
+    }
+  },
+  created(){
+    //调用渲染数据
+    this.getGoodsList()
+  },
+  methods: {
+    // 发送ajax, 拿数据
+    getGoodsList(){
+      this.$http.get('api/getgoods?pageindex=' + this.pageIndex).then(result => {
+        if (result.body.status === 0) {
+          this.goodsList =  this.goodsList.concat(result.body.message) 
+          console.log(this.goodsList)
+        }
+      })
+    },
+    getmore(){
+      this.pageIndex++
+      this.getGoodsList()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.goodslist-container {
+  .goods-list {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 8px;
+    justify-content: space-between;
+
+    .goods-item {
+      width: 49%;
+      min-height: 293px;
+      border: 1px solid #ccc;
+      box-shadow: 0 0 8px #ccc;
+      margin-bottom: 8px;
+      padding: 5px;
+
+      img {
+        width: 100%; 
+        min-height: 166.5px;
+      }
+
+      .info {
+        font-size: 12px;
+        .title {
+          font-size: 14px;
+          font-weight: 600;
+          min-height: 60px;
+          color: #444;
+          line-height: 20px;
+        }
+        .price {
+          min-height: 30px;
+          .new {
+            color: #f10215;
+            font-weight: 700;
+            font-size: 14px;
+          }
+          .old {
+            text-decoration: line-through;
+            margin-left: 5px;
+          }
+        }
+        .hot {
+          display: flex;
+          justify-content: space-between;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -9,6 +9,7 @@ import NewsList from './components/news/newsList.vue'
 import NewsInfo from './components/news/NewsInfo.vue'
 import PhotoList from './components/photo/PhotoList.vue'
 import PhotoInfo from './components/photo/PhotoInfo.vue'
+import GoodsList from './components/goods/GoodsList.vue'
 
 // 1-3 配置路由对象
 let router = new VueRouter({
@@ -21,7 +22,8 @@ let router = new VueRouter({
     {path: '/home/newsList', component: NewsList},
     {path: '/home/newsinfo/:id', component: NewsInfo},
     {path: '/home/photoList', component: PhotoList},
-    {path: '/home/photoinfo/:id', component: PhotoInfo}
+    {path: '/home/photoinfo/:id', component: PhotoInfo},
+    {path: '/home/goodslist', component: GoodsList}
   ],
   linkActiveClass: 'mui-active' //修改默认当前路由高亮的类, 改为mui写好的类
 })
